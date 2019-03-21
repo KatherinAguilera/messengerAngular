9cccd7852a1ba9dfafb16f4aa5455e9210a2fdcf
Katherine Aguilera <katherinam35@gmail.com>
2019-03-21 12:31:59 -0500

    Login Firebase

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthenticationService } from 'src/app/services/authentication.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   operation: string = 'login';
-  constructor() { }
+  public email: string = null;
+  public password: string = null;
+  // inyectar el servicio creado en authentication.ts
+  constructor( private authenticationService: AuthenticationService) { 
+    this.operation = 'login';
+  }
 
   ngOnInit() {
+  }
+  login() {
+    this.authenticationService.loginWithEmail(this.email, this.password).then(
+      (data) => {
+        alert('Loggeado correctamente');
+        console.log(data);
+      }
+    ).catch(
+      (error) => {
+        alert('Ocurrió un error');
+        console.log(error);
+      }
+    );
+  }
+  register() {
+    this.authenticationService.registerWithEmail(this.email, this.password)
+    .then(
+      (data) => {
+        alert('Registrado correctamente');
+        console.log(data);
+      }).catch(
+        (error) => {
+        alert('Ocurrió un error en el registro');
+        console.log(error);
+      });
   }
 
 }
---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor(private angularFireAuth: AngularFireAuth) {}
+
+  // loginSocial() {
+  //   return this.angularFireAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+  // }
+// Autenticacion con Email
+  loginWithEmail(email: string, password: string) {
+    return this.angularFireAuth.auth.signInWithEmailAndPassword(email, password);
+  }
+
+  registerWithEmail(email: string, password: string) {
+    return this.angularFireAuth.auth.createUserWithEmailAndPassword(email, password);
+  }
+// recuperar la sesion de usuario que esta logueado
+  getStatus() {
+    return this.angularFireAuth.authState;
+  }
+
+  logOut() {
+    return this.angularFireAuth.auth.signOut();
+  }
+}
