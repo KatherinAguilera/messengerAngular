59400a734bf4d1bcef01e878ff2ab3e221fff708
Katherine Aguilera <katherinam35@gmail.com>
2019-03-28 11:49:46 -0500

    Send messages firebase

---
+++ src/app/services/conversation.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {AngularFireDatabase} from '@angular/fire/database';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConversationService {
+// injectar firebase database
+  constructor(private angularFireDatabase: AngularFireDatabase) { }
+  createConversation(conversation) {
+    return this.angularFireDatabase.object('conversations/' + conversation.uid + '/' + conversation.timestamp).set(conversation);
+  }
+  // obtenerlas
+  getConversation(uid) {
+    return this.angularFireDatabase.list('conversations/' + uid);
+  }
+}
